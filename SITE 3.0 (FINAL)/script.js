a302fc6adabe77f6cce8e3edf2c9cea2650e5bba
cleitao018 <40722618+c221964@users.noreply.github.com>
2018-11-21 17:57:36 -0200

    Add files via upload

---
+++ SITE 3.0 (FINAL)/script.js
@@ -0,0 +1,18 @@
+function texto(){
+	var check = getElementById('check');
+	if (getElementById('check').checked == true) {
+		getElementById('texto').innerHTML = 'Ligado';
+	}
+}
+
+function delay(ms) {
+        var cur_d = new Date();
+        var cur_ticks = cur_d.getTime();
+        var ms_passed = 0;
+        while(ms_passed < ms) {
+            var d = new Date();  // Possible memory leak?
+            var ticks = d.getTime();
+            ms_passed = ticks - cur_ticks;
+            // d = null;  // Prevent memory leak?
+        }
+    }
