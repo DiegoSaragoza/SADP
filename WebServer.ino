3dd02b94f4879e6d8a012aeb5048cf9081bb13c0
cleitao018 <40722618+c221964@users.noreply.github.com>
2018-11-21 17:54:08 -0200

    Update WebServer.ino

--- WebServer.ino
+++ WebServer.ino
@@ -9,10 +9,34 @@ EthernetServer server(80);
 File webFile;
 char HTTP_req[REQ_BUF_SZ] = {0}; 
 char req_index = 0; 
+boolean c = false;
+void Bomba(boolean bomba)
+{
+  if (bomba = true)
+  {
+    digitalWrite(2, LOW);
+    delay(5000);
+    digitalWrite(2, HIGH);
+  }
+  else if (bomba = false){
+    digitalWrite(2,HIGH);
+  }
+}
+void Ciclo(boolean Ciclo)
+{
+    digitalWrite(2,LOW);
+    while (Ciclo = true){
+    digitalWrite(3, LOW);
+    delay(5000);
+    digitalWrite(3, HIGH);
+    delay(28800000);
+    }
+}
       
        
 void setup()
 {  
+    
     pinMode(2, OUTPUT);
     digitalWrite(2, HIGH);
     pinMode(10, OUTPUT);
@@ -35,7 +59,6 @@ void setup()
 }
 void loop()
 {
-    
     int lumi = analogRead(5); 
     EthernetClient client = server.available();
     if (client) {  
@@ -57,13 +80,28 @@ void loop()
                         client.println();
                         webFile = SD.open("index.htm");       
                     }
+                    else if (StrContains(HTTP_req, "GET /lon.htm") || StrContains(HTTP_req, "GET /loff.htm") || StrContains(HTTP_req, "GET /gerencia.htm") || StrContains(HTTP_req, "GET /home.htm") || StrContains(HTTP_req, "GET /boff.htm") || StrContains(HTTP_req, "GET /bon.htm") || StrContains(HTTP_req, "GET /son.htm")) {
+                      if (StrContains(HTTP_req, "GET /lon.htm")){
+                        digitalWrite(2, LOW);
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-Type: text/html");
+                        client.println("Connnection: close");
+                        client.println();
+                        webFile = SD.open("lon.htm");
+                      }else if (StrContains(HTTP_req, "GET /loff.htm")){
+                        digitalWrite(2, HIGH);
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-Type: text/html");
+                        client.println("Connnection: close");
+                        client.println();
+                        webFile = SD.open("loff.htm");
+                      }
                     else if (StrContains(HTTP_req, "GET /home.htm")) {
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-Type: text/html");
                         client.println("Connnection: close");
                         client.println();
                         webFile = SD.open("home.htm");        
-                        
                     }
                     else if (StrContains(HTTP_req, "GET /gerencia.htm")) {
                         client.println("HTTP/1.1 200 OK");
@@ -73,51 +111,34 @@ void loop()
                         webFile = SD.open("gerencia.htm");        
                         client.print("<p hidden id='dado'>" + String(lumi) + "</p>"); 
                     }
-                    else if (StrContains(HTTP_req, "GET /lon.htm") || StrContains(HTTP_req, "GET /loff.htm")) {
-                      if (StrContains(HTTP_req, "GET /lon.htm")){
-                        digitalWrite(2, LOW);
+                    else if (StrContains(HTTP_req, "GET /boff.htm")){
+                        boolean bomba = false;
+                        Bomba(bomba);
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-Type: text/html");
                         client.println("Connnection: close");
                         client.println();
-                        webFile = SD.open("lon.htm");  
+                        webFile = SD.open("boff.htm");
                       }
-                      else if (StrContains(HTTP_req, "GET /loff.htm")){
-                        digitalWrite(2, HIGH);
+                      else if (StrContains(HTTP_req, "GET /bon.htm")){
+                        boolean bomba = true;
+                        Bomba(bomba);
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-Type: text/html");
                         client.println("Connnection: close");
                         client.println();
-                        webFile = SD.open("loff.htm");  
-                      }
-                           
-                    }
-                    
-                    else if (StrContains(HTTP_req, "GET /script.js")) {
+                        webFile = SD.open("bon.htm");
+                      }   
+                      else if (StrContains(HTTP_req, "GET /son.htm")){
                         client.println("HTTP/1.1 200 OK");
-                        client.println("Content-Type: text/js");
+                        client.println("Content-Type: text/html");
                         client.println("Connnection: close");
                         client.println();
-                        webFile = SD.open("script.js");       
-                    }
-                    
-                    
-                    else if (StrContains(HTTP_req, "GET /style.css")) {
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-Type: text/css");
-                        client.println("Connnection: close");
-                        client.println();
-                        webFile = SD.open("style.css");        
-                    }
-                       
-                    else if (StrContains(HTTP_req, "GET /logo3.png")) {
-                        webFile = SD.open("logo3.png");
-                        if (webFile) {
-                            client.println("HTTP/1.1 200 OK");
-                            client.println();
-                         }
-                    }
-
+                        webFile = SD.open("son.htm");
+                        c = true;
+                        Ciclo(c);
+                      }                         
+                   }
                     if (webFile) {
                         while(webFile.available()) {
                             client.write(webFile.read()); 
